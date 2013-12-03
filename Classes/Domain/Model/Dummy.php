441c62192f83db6783b26e7e962f43961b549d62
Daniel Schöne <daniel@schoene.it>
2013-12-03 18:59:13 +0100

    initial import

---
+++ Classes/Domain/Model/Dummy.php
@@ -0,0 +1,38 @@
+<?php
+namespace Nb\NbVhsAssetsDependencyTest\Domain\Model;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Daniel Schöne <daniel@schoene.it>, schoene.it
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @package nb_vhs_assets_dependency_test
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class Dummy extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+
+}
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	'Nb.' . $_EXTKEY,
+	'Pi1',
+	array(
+		'Dummy' => 'index',
+		
+	),
+	// non-cacheable actions
+	array(
+		'Dummy' => '',
+		
+	)
+);
+
+?>
